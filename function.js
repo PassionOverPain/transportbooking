7f4e2e9b4bda5fa7af2c801b7e53d396ed572c63
Generic account User <129593058+PassionOverPain@users.noreply.github.com>
2024-09-30 12:33:06 +0200

    Added buttons alert

---
+++ function.js
@@ -0,0 +1,8 @@
+function Confirm() {
+  alert(
+    `Your booking has been confirmed. Please check your emails for a verification statement.`
+  );
+}
+function Reset() {
+  alert(`All your details are now Reset.`);
+}
